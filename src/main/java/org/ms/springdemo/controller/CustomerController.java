40d599f57805486a8d6c09790113fcde1203737a
mesutbaltaci <mesutbaltaci@hotmail.com>
2020-03-04 14:40:44 -0500

    add customer

--- src/main/java/org/ms/springdemo/controller/CustomerController.java
+++ src/main/java/org/ms/springdemo/controller/CustomerController.java
@@ -8,6 +8,8 @@ import org.ms.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,6 +32,13 @@ public class CustomerController {
 		if (theCustomer==null) {
 			throw new CustomerNotFoundException("Costomer id not found..." + customerId);
 		}
+		return theCustomer;
+	}
+	
+	@PostMapping("/customers") 
+	public Customer addCustomer(@RequestBody Customer theCustomer) {
+		theCustomer.setId(0);
+		customerService.saveCustomer(theCustomer);
 		return theCustomer;
 	}
 }
