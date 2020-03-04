c6f56be20f02025978dfb3d63cf2ae6dc7412bcd
mesutbaltaci <mesutbaltaci@hotmail.com>
2020-03-04 14:52:35 -0500

    delete customer

--- src/main/java/org/ms/springdemo/controller/CustomerController.java
+++ src/main/java/org/ms/springdemo/controller/CustomerController.java
@@ -6,6 +6,7 @@ import org.ms.springdemo.entity.Customer;
 import org.ms.springdemo.rest.CustomerNotFoundException;
 import org.ms.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,4 +49,17 @@ public class CustomerController {
 		customerService.saveCustomer(theCustomer);
 		return theCustomer;
 	}
+	
+	@DeleteMapping("/customers/{customerId}")
+	public String deleteCustomer(@PathVariable int customerId) {
+		Customer theCustomer = customerService.getCustomer(customerId);
+		if (theCustomer==null) {
+			throw new CustomerNotFoundException("Costomer id not found..." + customerId);
+		}
+		customerService.deleteCustomer(customerId);
+		return "Deleted customer id - " + customerId;
+	}
+	
+	
+	
 }
